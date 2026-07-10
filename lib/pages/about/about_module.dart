07c3e46a79ac031478e00ac88ba578ae5d5fdbfe
星荧梦 <wangziheng8@163.com>
2026-07-10 21:04:50 +0800

    fix: import Material license page

--- lib/pages/about/about_module.dart
+++ lib/pages/about/about_module.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:kazumi/pages/about/about_page.dart';
 import 'package:kazumi/pages/logs/logs_page.dart';
